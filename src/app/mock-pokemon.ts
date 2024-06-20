aaa0cdbe9bebbeccc50895f3d614d1f4a7949ad0
ucacloud <mgcnlrar@gmail.com>
2024-06-20 01:48:02 -0500

    pokemon list

---
+++ src/app/mock-pokemon.ts
@@ -0,0 +1,34 @@
+import { Pokemons } from "./pokemons";
+
+export const POKEMONS: Pokemons[] = [
+  {
+    id: 'f97e0065-a7af-434b-b1f1-e7c089beb619',
+    name: 'Bulbasaur',
+    type: 'Grass',
+    weight: 10
+  },
+  {
+    id: 'a2937bc4-d1f9-4c4c-b5d1-4c58f675fc5e',
+    name: 'Squirtle',
+    type: 'Water',
+    weight: 7
+  },
+  {
+    id: '9dda100a-5935-46b2-b810-249a6151d647',
+    name: 'Charmander',
+    type: 'Fire',
+    weight: 8
+  },
+  {
+    id: 'db6871bb-5fcb-4d0e-8521-d1aa4c52442e',
+    name: 'Caterpie',
+    type: 'Bug',
+    weight: 3
+  },
+  {
+    id: 'fcb5b429-5f3b-4bdd-b0c7-3b85288a9f09',
+    name: 'Weedle',
+    type: 'Bug',
+    weight: 4
+  }
+]
--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -1,18 +1,23 @@
 import { Component } from '@angular/core';
 import { Pokemons } from '../pokemons';
+import { FormsModule } from '@angular/forms';
+import { POKEMONS } from '../mock-pokemon';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-pokemon',
   standalone: true,
-  imports: [],
+  imports: [FormsModule, NgFor],
   templateUrl: './pokemon.component.html',
   styleUrl: './pokemon.component.css'
 })
 export class PokemonComponent {
   pokemons: Pokemons = {
     id: '3f9451b-2ac1-4497-be1c-b3b86facc9d3',
-    name: 'Bulbasaur #1',
+    name: 'Bulbasaur',
     type: 'Grass',
     weight: 10
   };
+
+  pokemon = POKEMONS;
 }
