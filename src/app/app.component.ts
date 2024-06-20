b4b971a1f1e52e95d8eeb335120748c6ba70e57e
ucacloud <mgcnlrar@gmail.com>
2024-06-20 00:25:08 -0500

    quick save

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { PokemonComponent } from './pokemon/pokemon.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, PokemonComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
