4a0346cadb978aad4d5898320d904787ec1e4b9a
ucacloud <mgcnlrar@gmail.com>
2024-06-25 01:26:25 -0500

    added techStack and resume links

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,6 +3,8 @@ import { PokemonComponent } from './pokemon/pokemon.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { PokemonsDetailComponent } from './pokemons-detail/pokemons-detail.component';
 import { PokemonCreateComponent } from './pokemon-create/pokemon-create.component';
+import { TechStackComponent } from './tech-stack/tech-stack.component';
+import { ResumeComponent } from './resume/resume.component';
 
 export const routes: Routes = [
   { path: 'pokemon', component: PokemonComponent },
@@ -10,4 +12,6 @@ export const routes: Routes = [
   { path: '', redirectTo: '/dashboard', pathMatch: 'full'},
   { path: 'pokemon/:id', component: PokemonsDetailComponent},
   { path: 'pokemon-create', component: PokemonCreateComponent},
+  { path: 'techStack', component: TechStackComponent},
+  { path: 'resume', component: ResumeComponent},
 ];
